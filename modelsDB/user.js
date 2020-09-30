4fbeac5fb957e51538d6e73fd113ea1676f12307
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-30 19:39:21 +0300

    user model mongo

--- modelsDB/user.js
+++ modelsDB/user.js
@@ -1,7 +1,33 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
-/** 
- * TODO: 
- * Make a simple registration 
- * Implement authorization using JWT
- */
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    checkPassword(value) {
+      let options = { ignore_whitespace: true }
+      if(validator.isEmpty(value, options)) {
+        throw new Error('Enter you password!')
+      } 
+    },
+  },
+  tokens: [{
+    token: {
+      type: String,
+      // required: true,
+    },
+  },],
+}, { 
+  timestamps: { 
+    createdAt: 'created_at' 
+  }
+})
+
+const User = mongoose.model('User', UserSchema)
+
+exports.User = User
