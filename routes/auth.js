d83a4cede04c596f6e0dde785675bfcd7675bc0a
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-30 20:38:13 +0300

    Implemented authorization with jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,31 @@
 const Router = require('express').Router()
+const jwt = require('jsonwebtoken')
+const { User } = require('../modelsDB/user')
 
-Router.post('/auth', (req, res) => {
-  res.send('1')
+async function auth(req, res, next) {
+  const token = req.header('Authorization').replace('Bearer', '').trim()
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, "super_secret_key")
+  } catch (error) {
+    res.status(400).send('BAD REQUEST')
+    return
+  }
+
+  User.findById(decoded.id, (err, doc) => {
+    if(err) {
+      res.status(401).send('UNAUTHORIZED')
+      return
+    }
+
+    req.user = doc
+    next()
+  })
+}
+
+Router.post('/auth', auth, (req, res) => {
+  res.json(req.user)
 })
 
 exports.Router = Router
