3e499896a027b321776c81c5e65cda30d2a180ce
loveyousomuch <loveyousomuch554@gmail.com>
2020-10-01 23:27:15 +0300

    authorization with redis

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,20 @@
 const Router = require('express').Router()
 const jwt = require('jsonwebtoken')
+const redis = require('redis')
 const { User } = require('../modelsDB/user')
+const { Session } = require('../Session')
+const { v4: uuidv4 } = require('uuid')
+const config = require('config')
 
-async function auth(req, res, next) {
+const redisClient = redis.createClient({
+  host: config.get('host'),
+  port: config.get('port'),
+  password: config.get('password')
+})
+
+function auth(req, res, next) {
   const token = req.header('Authorization').replace('Bearer', '').trim()
+  const sessionToken = req.header('sessiontoken')
   let decoded;
 
   try {
@@ -13,11 +24,33 @@ async function auth(req, res, next) {
     return
   }
 
-  User.findById(decoded.id, (err, doc) => {
+  User.findById(decoded.id, async (err, doc) => {
     if (err) {
       res.status(401).send('UNAUTHORIZED')
       return
     }
+
+    /**
+     * If the session has expired, create a new session; 
+     * otherwise, add the existing session to the request(req)
+     */
+    let isExit;
+    await isSessionExistAndSetSession(req, sessionToken)
+      .then((isSessionExist) => {
+        if (!isSessionExist) {
+          createSession(req, doc)
+        }
+      })
+      .catch((err) => {
+        isExit = true
+
+        if (err === 'Invalid session token') {
+          res.status(401).send('Invalid session token')
+          return
+        }
+        res.status(500).send('INTERNAL SERVER ERROR')
+      })
+      if (isExit) return; 
 
     req.user = doc
     next()
@@ -25,7 +58,51 @@ async function auth(req, res, next) {
 }
 
 Router.post('/auth', auth, (req, res) => {
-  res.json(req.user)
+  res.setHeader('sessiontoken', req.sessionId)
+  res.json({
+    user: req.user,
+    token: req.sessionId,
+  })
 })
+
+function isSessionExistAndSetSession(req, sessionToken) {
+  return new Promise((resolve, reject) => {
+    if (!sessionToken) {
+      reject('Invalid session token')
+    }
+
+    redisClient.get(sessionToken, (err, user) => {
+      if (err) {
+        reject(err)
+      }
+      
+      if (!user) {
+        resolve(false)
+      }
+
+      req.session = new Session(sessionToken, user)
+      req.sessionId = sessionToken
+      resolve(true)
+    })
+  })
+}
+
+function createSession(req, user) {
+  let sessionId = uuidv4()
+
+  // Create session object
+  const currentSession = new Session(sessionId, user)
+
+  // Add session and session id to the request 
+  req.session = currentSession
+  req.sessionId = sessionId
+
+  // Save session in redis
+  try {
+    currentSession.save(redisClient)
+  } catch (error) {
+    return error
+  }
+}
 
 exports.Router = Router
